0dc106feddc9b7418f6a08b3c301d94bc01d05b5
codereader <greebo@angua.at>
2023-10-14 16:47:31 +0200

    Initial commit

---
+++ RockPaperScissors/Extensions.cs
@@ -0,0 +1,44 @@
+﻿namespace RockPaperScissors
+{
+    internal static class Extensions
+    {
+        public static char GetOutCome(this string fight) => GetOutCome(fight[0], fight[1]);
+
+        public static char GetOutCome(char first, char second)
+        {
+            if (first == second)
+            {
+                return first;
+            }
+
+            switch (first)
+            {
+                case 'R':
+                    return second switch
+                    {
+                        'P' => 'P', // paper beats rock
+                        'S' => 'R', // rock beats scissors
+                        _ => first
+                    };
+
+                case 'P':
+                    return second switch
+                    {
+                        'R' => 'P', // paper beats rock
+                        'S' => 'S', // scissors beats paper
+                        _ => first
+                    };
+
+                case 'S':
+                    return second switch
+                    {
+                        'P' => 'S', // scissors beats paper
+                        'R' => 'R', // rock beats scissors
+                        _ => first
+                    };
+            }
+
+            throw new ArgumentOutOfRangeException("Invalid fighting style");
+        }
+    }
+}
---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using RockPaperScissors;
+
+namespace Contest;
+
+public class Program
+{
+	public static void Main(string[] args)
+    {
+        //Level1();
+        //Level2();
+        Level3();
+
+        Console.WriteLine("Done");
+    }
+
+
+    private static void Level3()
+    {
+        for (var inputFileNumber = 1; inputFileNumber <= 5; inputFileNumber++)
+        {
+            var inputfilename = $"../../../level3_{inputFileNumber}.in";
+            var outputfilename = $"../../../level3_{inputFileNumber}.out";
+
+            var lines = File.ReadAllLines(inputfilename).ToList();
+            var tournaments = lines.Skip(1).ToList();
+
+            using var outputWriter = new StreamWriter(outputfilename);
+
+            foreach (var input in tournaments) 
+            {
+                var line = input.Replace('R', ' ');
+                line = line.Replace('P', ' ');
+                line = line.Replace('S', ' ');
+
+                var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var rocks = int.Parse(parts[0]);
+                var papers = int.Parse(parts[1]);
+                var scissors = int.Parse(parts[2]);
+
+                List<string> pairs = new List<string>();
+
+                // Avoid pairing a single scissor with a rock
+                if (scissors == 1 && papers > 0)
+                {
+                    pairs.Add("SP");
+                    scissors--;
+                    papers--;
+                }
+
+                // Pair Rocks with Paper
+                var rpPairs = Math.Min(rocks, papers);
+                pairs.AddRange(Enumerable.Range(1, rpPairs).Select(_ => "RP"));
+                rocks -= rpPairs;
+                papers -= rpPairs;
+
+                // Pair remaining Papers with Scissors
+                var spPairs = Math.Min(papers, scissors);
+                pairs.AddRange(Enumerable.Range(1, spPairs).Select(_ => "SP"));
+                scissors -= spPairs;
+                papers -= spPairs;
+
+                var rrPairs = Math.DivRem(rocks, 2, out rocks);
+                pairs.AddRange(Enumerable.Range(1, rrPairs).Select(_ => "RR"));
+
+                if (rocks > 1) throw new InvalidOperationException("Too many rocks error");
+
+                if (rocks == 1 && scissors > 0)
+                {
+                    pairs.Add("RS");
+                    rocks--;
+                    scissors--;
+                }
+
+                if (rocks > 0) throw new InvalidOperationException("Too many rocks error");
+
+                // Match remaining Papers with themselves
+                var ppPairs = Math.DivRem(papers, 2, out papers);
+                pairs.AddRange(Enumerable.Range(1, ppPairs).Select(_ => "PP"));
+
+                // Match remaining scissors with themselves
+                var ssPairs = Math.DivRem(scissors, 2, out scissors);
+                pairs.AddRange(Enumerable.Range(1, ssPairs).Select(_ => "SS"));
+
+                if (scissors > 0 || papers > 0 || rocks > 0) throw new InvalidOperationException("Logic error");
+
+                pairs.Sort();
+
+                for (int i = 0; i < pairs.Count; i++)
+                {
+                    if (i == 0 || pairs[i - 1] == "RP")
+                    {
+                        if (TryFindPairToTheRight("RS", i, out var index))
+                        {
+                            SwapPositions(i, index);
+                            continue;
+                        }
+                        
+                        if (TryFindPairToTheRight("RR", i, out index))
+                        {
+                            SwapPositions(i, index);
+                            continue;
+                        }
+                    }
+                }
+
+                Console.WriteLine(string.Join(" ", pairs));
+
+                var lineup = string.Join("", pairs);
+                outputWriter.WriteLine(lineup);
+
+                var tournamentResult = RunTournamentForRounds(lineup, 2);
+
+                if (tournamentResult.Contains('R')) throw new InvalidOperationException("No rocks allowed here");
+                if (!tournamentResult.Contains('S')) throw new InvalidOperationException("No scissors left");
+
+                Console.WriteLine("After two rounds: {0}", tournamentResult);
+
+                bool TryFindPairToTheRight(string pair, int startIndex, out int index)
+                {
+                    index = pairs.IndexOf(pair, startIndex);
+                    return index != -1;
+                }
+
+                void SwapPositions(int index1, int index2)
+                {
+                    var temp = pairs[index1];
+                    pairs[index1] = pairs[index2];
+                    pairs[index2] = temp;
+                }
+            }
+
+#if false
+            
+
+            foreach (var tournament in tournaments)
+            {
+                string fighters = RunTournamentForRounds(tournament, 2);
+
+                outputWriter.WriteLine(fighters);
+            } 
+#endif
+        }
+    }
+
+
+    private static void Level2()
+    {
+        for (var i = 1; i <= 5; i++)
+        {
+            var inputfilename = $"../../../level2_{i}.in";
+            var outputfilename = $"../../../level2_{i}.out";
+
+            var lines = File.ReadAllLines(inputfilename).ToList();
+            var tournaments = lines.Skip(1).ToList();
+            using var outputWriter = new StreamWriter(outputfilename);
+
+            foreach (var tournament in tournaments)
+            {
+                var fighters = RunTournamentForRounds(tournament, 2);
+
+                outputWriter.WriteLine(fighters);
+            }
+        }
+    }
+
+    private static string RunTournamentForRounds(string tournament, int numRounds)
+    {
+        var fighters = tournament;
+
+        for (int round = 0; round < numRounds; round++)
+        {
+            var roundFighters = fighters.ToArray();
+            var roundResult = new StringBuilder();
+
+            for (int f = 0; f < roundFighters.Length; f += 2)
+            {
+                roundResult.Append(Extensions.GetOutCome(roundFighters[f], roundFighters[f + 1]));
+            }
+
+            fighters = roundResult.ToString();
+        }
+
+        return fighters;
+    }
+
+    private static void Level1()
+    {
+        for (var i = 1; i <= 5; i++)
+        {
+            var inputfilename = $"../../../level1_{i}.in";
+            var outputfilename = $"../../../level1_{i}.out";
+
+            var lines = File.ReadAllLines(inputfilename).ToList();
+            var fights = lines.Skip(1).ToList();
+
+            var result = new StringBuilder();
+
+            //result.AppendLine(fights.Count.ToString());
+
+            foreach (var fight in fights)
+            {
+                result.AppendLine(fight.GetOutCome().ToString());
+            }
+
+            File.WriteAllText(outputfilename, result.ToString());
+        }
+    }
+}
